853ddfb5f2815a57848d8f5983abc0f7160c66a2
mpr <marinpericradan1@gmail.com>
2025-07-09 21:25:16 +0200

    chore: config updates

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,6 @@
 import fs from "fs";
+import os from "os"
+import path from "path"
 
 export type Config = {
   dbUrl: string;
@@ -6,8 +8,15 @@ export type Config = {
 }
 
 export function setUser(userName: string) {
-  console.warn("DEBUGPRINT[59]: config.ts:8: userName=", userName)
   const gatorConfigFile = fs.readFileSync(".gatorconfig.json", "utf8")
-  console.warn("DEBUGPRINT[58]: config.ts:9: gatorConfigFile=", gatorConfigFile)
+  const gatorConfigObj = JSON.parse(gatorConfigFile)
 
+  const newConfig: Config = {
+    dbUrl: gatorConfigObj.db_url,
+    currentUserName: userName
+  }
+
+
+  const pathToWrite = path.join(os.homedir(), 'playground/gator-ts/.gatorconfig.json')
+  fs.writeFileSync(pathToWrite, JSON.stringify(newConfig))
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
+import { setUser } from "./config"
+import fs from "fs"
+import os from "os"
+import path from "path"
+
 function main() {
-  console.log("Hello, world!")
+  setUser("mpr")
+  const configFilePath = path.join(os.homedir(), 'playground/gator-ts/.gatorconfig.json')
+  const configContents = fs.readFileSync(configFilePath, "utf8")
+
+  console.log(configContents)
+
 }
 
 main()
