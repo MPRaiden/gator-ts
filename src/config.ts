def1b8e4fabe2d5d57d6cb4fa6b1704526e96c7a
Marin Peric Radan <marin.peric@seekandhit.com>
2025-07-11 08:22:56 +0200

    config updated

--- src/config.ts
+++ src/config.ts
@@ -1,22 +1,61 @@
 import fs from "fs";
-import os from "os"
-import path from "path"
+import os from "os";
+import path from "path";
 
-export type Config = {
+type Config = {
   dbUrl: string;
   currentUserName: string;
-}
+};
 
 export function setUser(userName: string) {
-  const gatorConfigFile = fs.readFileSync(".gatorconfig.json", "utf8")
-  const gatorConfigObj = JSON.parse(gatorConfigFile)
+  const config = readConfig();
+  config.currentUserName = userName;
+  writeConfig(config);
+}
 
-  const newConfig: Config = {
-    dbUrl: gatorConfigObj.db_url,
-    currentUserName: userName
+function validateConfig(rawConfig: any) {
+  if (!rawConfig.db_url || typeof rawConfig.db_url !== "string") {
+    throw new Error("db_url is required in config file");
+  }
+  if (
+    !rawConfig.current_user_name ||
+    typeof rawConfig.current_user_name !== "string"
+  ) {
+    throw new Error("current_user_name is required in config file");
   }
 
+  const config: Config = {
+    dbUrl: rawConfig.db_url,
+    currentUserName: rawConfig.current_user_name,
+  };
 
-  const pathToWrite = path.join(os.homedir(), 'playground/gator-ts/.gatorconfig.json')
-  fs.writeFileSync(pathToWrite, JSON.stringify(newConfig))
+  return config;
 }
+
+export function readConfig() {
+  const fullPath = getConfigFilePath();
+
+  const data = fs.readFileSync(fullPath, "utf-8");
+  const rawConfig = JSON.parse(data);
+
+  return validateConfig(rawConfig);
+}
+
+function getConfigFilePath() {
+  const configFileName = ".gatorconfig.json";
+  const homeDir = os.homedir();
+  return path.join(homeDir, configFileName);
+}
+
+function writeConfig(config: Config) {
+  const fullPath = getConfigFilePath();
+
+  const rawConfig = {
+    db_url: config.dbUrl,
+    current_user_name: config.currentUserName,
+  };
+
+  const data = JSON.stringify(rawConfig, null, 2);
+  fs.writeFileSync(fullPath, data, { encoding: "utf-8" });
+}
+
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
-import { setUser } from "./config"
-import fs from "fs"
-import os from "os"
-import path from "path"
+import { readConfig, setUser } from "./config";
 
 function main() {
-  setUser("mpr")
-  const configFilePath = path.join(os.homedir(), 'playground/gator-ts/.gatorconfig.json')
-  const configContents = fs.readFileSync(configFilePath, "utf8")
-
-  console.log(configContents)
-
+  setUser("mpr");
+  const cfg = readConfig();
+  console.log(cfg);
 }
 
-main()
+main();
+
